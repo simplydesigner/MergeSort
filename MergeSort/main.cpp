c91eb1e7c228cc01cc943cef189422f44dd8e785
simplydesigner <simplydesigner@ya.ru>
2016-07-27 09:31:29 +0300

    Initial Commit

---
+++ MergeSort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MergeSort
+//
+//  Created by simplydesigner on 7/27/16.
+//  Copyright © 2016 simplydesigner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
